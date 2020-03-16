e3b159db8a07d27017cff1d2053bb23b29758ab5
Oriol Burgaya <oriolbv@gmail.com>
2020-03-16 20:24:51 +0100

    Survivor movement with wasd implemented

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public Animator animator;
+
+    public float moveSpeed;
     void Start()
     {
-        Debug.Log("Holi");
         animator = GetComponent<Animator>();
+
+        moveSpeed = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
             animator.SetBool("isMoving", true);
+            transform.Translate(moveSpeed * (Input.GetAxis("Horizontal") * Time.deltaTime), 0f, moveSpeed * (Input.GetAxis("Vertical") * Time.deltaTime));
         }
         else
         {
