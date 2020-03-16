1dd77a313333f3e6416a0ce024c96b60e4312cee
Oriol Burgaya <oriolbv@gmail.com>
2020-03-16 19:49:30 +0100

    Scene and player added

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Holi");
+        //this.GetComponent<Animator>().Play("Animations_Static.Run_Static");
+        //animator.GetComponent<EnemyScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Animator>().Play("Animations_Static.Run_Static");
+    }
+}
